efe92c0fca7bbf0e83dba367f906cf0ac4eafb8a
Stepan <takistepan@gmail.com>
2023-02-02 18:39:51 +0300

    Реализована возможность удаления поста его автором

--- src/App.js
+++ src/App.js
@@ -253,7 +253,11 @@ function App() {
       </div>
       <div className="app_posts">
         {posts?.map((post) => (
-          <Post post={post} />
+          <Post
+            post={post}
+            authToken={authToken}
+            authTokenType={authTokenType}
+          />
         ))}
       </div>
       {authToken ? (
--- src/Post.js
+++ src/Post.js
@@ -4,7 +4,7 @@ import { Avatar, Button } from "@material-ui/core";
 
 const BASE_URL = "http://localhost:8000/";
 
-function Post({ post }) {
+function Post({ post, authToken, authTokenType }) {
   const [imageUrl, setImageUrl] = useState("");
   const [comments, setComments] = useState([]);
 
@@ -20,6 +20,28 @@ function Post({ post }) {
     setComments(post.comments);
   }, []);
 
+  const handleDelete = (event) => {
+    event?.preventDefault();
+
+    const requestOptions = {
+      method: "GET",
+      headers: new Headers({
+        Authorization: authTokenType + " " + authToken,
+      }),
+    };
+
+    fetch(BASE_URL + "post/delete/" + post.id, requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          window.location.reload();
+        }
+        throw response;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="post">
       <div className="post_header">
@@ -27,7 +49,9 @@ function Post({ post }) {
       </div>
       <div className="post_headerInfo">
         <h3>{post.user.username}</h3>
-        <Button className="post_delete">Удалить</Button>
+        <Button className="post_delete" onClick={handleDelete}>
+          Удалить
+        </Button>
       </div>
       <img className="post_image" src={imageUrl} />
       <h4 className="post_text">{post.caption}</h4>
